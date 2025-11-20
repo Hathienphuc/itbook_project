3d82166297a00371e4c1a3741b49769303e25981
hathienphuck15 <hathienphuc12a8@gmail.com>
2025-11-20 15:13:20 +0700

    Cập nhật file kết nối database và thêm Procfile

--- Model/ketnoi.php
+++ Model/ketnoi.php
@@ -2,18 +2,37 @@
     class clsketnoi {
         // Hàm kết nối CSDL
         function ketnoiDB(& $con) {
-            $con = mysqli_connect("localhost", "root", "");
-            mysqli_set_charset($con, "utf8");
-            if($con){
-                return mysqli_select_db($con, "itbook");
-            }else{
-                return false;
-            }
+        $cleardb_url = getenv("CLEARDB_DATABASE_URL");
+
+        if ($cleardb_url) {
+            // Nếu chạy trên Heroku (ClearDB)
+            $url = parse_url($cleardb_url);
+
+            $server = $url["host"];
+            $user = $url["user"];
+            $pass = $url["pass"];
+            $db = ltrim($url["path"], '/');
+        } else {
+            // Chạy local (WAMP)
+            $server = "localhost";
+            $user = "root";
+            $pass = "";
+            $db = "itbook";
         }
 
-        // Hàm đóng kết nối CSDL
-        function dongketnoi($con) {
-            mysqli_close($con);
+        $con = mysqli_connect($server, $user, $pass);
+        mysqli_set_charset($con, "utf8");
+
+        if ($con) {
+            return mysqli_select_db($con, $db);
+        } else {
+            return false;
         }
+    }
+
+    // Hàm đóng kết nối CSDL
+    function dongketnoi($con) {
+        mysqli_close($con);
+    }
     }
 ?>
