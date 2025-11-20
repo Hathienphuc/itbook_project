eee7629778d0d209eb8930b0655cc444b4cae990
hathienphuck15 <hathienphuc12a8@gmail.com>
2025-11-20 15:44:30 +0700

    first commit

--- Model/ketnoi.php
+++ Model/ketnoi.php
@@ -2,37 +2,18 @@
     class clsketnoi {
         // Hàm kết nối CSDL
         function ketnoiDB(& $con) {
-        $cleardb_url = getenv("CLEARDB_DATABASE_URL");
-
-        if ($cleardb_url) {
-            // Nếu chạy trên Heroku (ClearDB)
-            $url = parse_url($cleardb_url);
-
-            $server = $url["host"];
-            $user = $url["user"];
-            $pass = $url["pass"];
-            $db = ltrim($url["path"], '/');
-        } else {
-            // Chạy local (WAMP)
-            $server = "localhost";
-            $user = "root";
-            $pass = "";
-            $db = "itbook";
+            $con = mysqli_connect("localhost", "root", "");
+            mysqli_set_charset($con, "utf8");
+            if($con){
+                return mysqli_select_db($con, "itbook");
+            }else{
+                return false;
+            }
         }
 
-        $con = mysqli_connect($server, $user, $pass);
-        mysqli_set_charset($con, "utf8");
-
-        if ($con) {
-            return mysqli_select_db($con, $db);
-        } else {
-            return false;
+        // Hàm đóng kết nối CSDL
+        function dongketnoi($con) {
+            mysqli_close($con);
         }
-    }
-
-    // Hàm đóng kết nối CSDL
-    function dongketnoi($con) {
-        mysqli_close($con);
-    }
     }
 ?>
